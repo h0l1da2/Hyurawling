56f2ed08bf69b1be506e6f7dc72d36c98389b2a1
h0l1da2 <whaleee@naver.com>
2023-09-23 13:32:58 +0900

    feat: First Commit

---
+++ src/main/java/app/rakuten/hyurawling/HyurawlingApplication.java
@@ -0,0 +1,13 @@
+package app.rakuten.hyurawling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyurawlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyurawlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/rakuten/hyurawling/HyurawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package app.rakuten.hyurawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyurawlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
