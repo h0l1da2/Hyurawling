b82f5dfa7e7f1c783ee9336c0b4c63e368c80e45
h0l1da2 <whaleee@naver.com>
2023-10-11 14:07:31 +0900

    test: 아이템 가져오기 테스트
    아이템 가져오기 테스트용 클래스 추가
    이걸로 사이트 테스트 해야함

--- src/test/java/app/rakuten/hyurawling/service/CrawlingServiceImplTest.java
+++ src/test/java/app/rakuten/hyurawling/service/CrawlingServiceImplTest.java
@@ -1,4 +1,24 @@
+package app.rakuten.hyurawling.service;
+
+import app.rakuten.hyurawling.vo.ProgramFields;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CrawlingServiceImplTest {
-  
+
+    private CrawlingService crawlingService = new CrawlingServiceImpl();
+
+    @Test
+    void crawling() throws IOException {
+
+        ProgramFields programFields = ProgramFields.builder()
+                .url("https://search.rakuten.co.jp/search/mall/shoes/")
+                .build();
+
+        crawlingService.crawling(programFields);
+
+    }
 }
